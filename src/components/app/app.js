c1972c52f7d52496244b414b17d440e0c80f1bd4
linata81 <linata81@gmail.com>
2021-10-04 18:42:09 +0300

    router

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,7 +3,9 @@ import { Col, Row, Container } from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ErrorMessage from '../errorMessage';
-import { CharacterPage, HousesPage, BooksPage } from '../pages';
+import { CharacterPage, HousesPage, BooksPage, BooksItem } from '../pages';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './app.css';
 export default class App extends Component {
 
     state = {
@@ -33,28 +35,36 @@ export default class App extends Component {
         }
 
         return (
-            <>
-                <Container>
-                    <Header />
-                </Container>
-                <Container>
-                    <Row>
-                        <Col lg={{ size: 5, offset: 0 }}>
-                            {char}
-                            <button
-                                type="button"
-                                className="btn btn-primary"
-                                style={{ marginBottom: '40px' }}
-                                onClick={this.toggleRandomChar}>
-                                Toggle random character
-                            </button>
-                        </Col>
-                    </Row>
-                    <CharacterPage />
-                    <HousesPage />
-                    <BooksPage />
-                </Container>
-            </>
+            <Router>
+                <div className="app">
+                    <Container>
+                        <Header />
+                    </Container>
+                    <Container>
+                        <Row>
+                            <Col lg={{ size: 5, offset: 0 }}>
+                                {char}
+                                <button
+                                    type="button"
+                                    className="btn btn-primary"
+                                    style={{ marginBottom: '40px' }}
+                                    onClick={this.toggleRandomChar}>
+                                    Toggle random character
+                                </button>
+                            </Col>
+                        </Row>
+                        <Route path='/characters' component={CharacterPage} />
+                        <Route path='/houses' component={HousesPage} />
+                        <Route path='/books' exact component={BooksPage} />
+                        <Route path='/books/:id' render={
+                            ({ match }) => {
+                                const { id } = match.params;
+                                return <BooksItem bookId={id} />
+                            }
+                        } />
+                    </Container>
+                </div>
+            </Router>
         );
     }
 };
--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,8 +1,10 @@
 import CharacterPage from './characterPage';
 import HousesPage from './housesPage';
 import BooksPage from './booksPage';
+import BooksItem from './booksItem';
 export {
   CharacterPage,
   HousesPage,
   BooksPage,
+  BooksItem
 }
